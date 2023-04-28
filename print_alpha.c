7800cbc4113480a5f1a5b36989fbd4ebb55313ef
Sonnie12 <dorcusmwangi2005@gmail.com>
2023-04-29 04:56:45 +0530

    print characters and strings

--- print_alpha.c
+++ print_alpha.c
@@ -1,25 +1,34 @@
 #include "main.h"
 
 /**
- * print_address - prints address of input in hexa format
+ * print_string - loops through a string and prints
+ * every character
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_address(va_list l, flags_t *f)
+int print_string(va_list l, flags_t *f)
 {
-	char *str;
-	unsigned long int p = va_arg(l, unsigned long int);
-
-	register int count = 0;
+	char *s = va_arg(l, char *);
 
 	(void)f;
 
-	if (!p)
-		return (_puts("(nil)"));
-	str = convert(p, 16, 1);
-	count += _puts("0x");
-	count += _puts(str);
-	return (count);
+	if (!s)
+		s = "(null)";
+	return (_puts(s));
+}
+
+/**
+ * print_char - prints a character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_char(va_list l, flags_t *f)
+{
+	(void)f;
+	_putchar(va_arg(l, int));
+	return (1);
 }
